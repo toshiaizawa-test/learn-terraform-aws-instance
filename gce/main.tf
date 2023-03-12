0e2ce1fef1f219d7cf3a6ae1ce041d3a40c59c52
Toshi Aizawa <toshi@eigocoach.net>
2023-03-13 05:33:08 +0900

    update gce/main.tf

--- gce/main.tf
+++ gce/main.tf
@@ -8,6 +8,9 @@ resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "e2-micro"
 
+  # tag
+  tags = ["foo", "bar"]
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
