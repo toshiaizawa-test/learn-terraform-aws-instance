fc28a1e9a7dd96e98046167d76e79491f8952cbd
Toshi Aizawa <toshi@eigocoach.net>
2023-03-16 07:42:18 +0900

    update gcs and s3

--- gcs/main.tf
+++ gcs/main.tf
@@ -1,18 +1,8 @@
 resource "google_storage_bucket" "static-site" {
-  name          = "gcs2.toshiaizawa.com"
+  name          = "gcstest-toshiaizawa"
   location      = "ASIA"
   force_destroy = true
 
   uniform_bucket_level_access = true
 
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://gcs2.toshiaizawa.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
-  }
 }
